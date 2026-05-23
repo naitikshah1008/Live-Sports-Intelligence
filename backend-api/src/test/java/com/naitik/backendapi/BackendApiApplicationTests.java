d63bfe4f4d28813b30470659a69c083d311b792a
naitikshah1008 <naitikshah053@gmail.com>
2026-05-23 13:19:26 -0700

    restored backend Spring Boot test setup

--- backend-api/src/test/java/com/naitik/backendapi/BackendApiApplicationTests.java
+++ backend-api/src/test/java/com/naitik/backendapi/BackendApiApplicationTests.java
@@ -1,13 +1,14 @@
-package com.naitik.backend_api;
+package com.naitik.backendapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
